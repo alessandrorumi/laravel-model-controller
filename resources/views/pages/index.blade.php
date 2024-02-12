f9b856fa7d547e4db64e2faee5563d5484f4884f
alessandrorumi <rumi.alessandro21@gmail.com>
2024-02-12 15:04:07 +0100

    Add: Movie via MySQLi

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,24 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+<div class="container d-flex flex-wrap justify-content-center">
+  @foreach ($movies as $movie)
+    <div class="card col-3 m-3">
+      <h4>{{$movie->title}}</h4>
+      <h5>{{$movie->original_title}}</h5>
+      <div class="info d-flex my-3 justify-content-around">
+        <div class="nationality">
+          {{$movie->nationality}}
+        </div>
+        <div class="date">
+          {{$movie->date}}
+        </div>
+      </div>
+      <div class="vote">
+        {{$movie->vote}}
+      </div>
+    </div>
+  @endforeach
+</div>
 @endsection
+
